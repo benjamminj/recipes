e70877a2dd67f7a177b694368ff96c56f7517f88
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-11-09 17:31:50 -0800

    Get zeit now deployment working w/ DB connnection!!!

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,8 @@ let pg = require('pg')
 // TODO: can we remove or senable?
 pg.defaults.ssl = true
 
+process.env.DATABASE_URL
+
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
