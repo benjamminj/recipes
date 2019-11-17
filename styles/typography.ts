2a18f7e80dd3318cc2abae011becde9a02e0c5ee
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-11-16 23:30:03 -0800

    Add color / typography / spacing systems

---
+++ styles/typography.ts
@@ -0,0 +1,16 @@
+export let primaryFont =
+  'font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif'
+
+export let fontSizes = {
+  h1: '4.5rem',
+  h2: '3.75rem',
+  h3: '3rem',
+  h4: '2.25rem',
+  h5: '1.875rem',
+  h6: '1.5rem',
+  xl: '1.25rem',
+  l: '1.125rem',
+  m: '1rem',
+  s: '0.875rem',
+  xs: '0.75rem',
+}
